9fb8d649e095bfed54a1cdccb9d476f70a317e4b
Webert Maximiano <webertcoach@gmail.com>
2023-05-25 19:31:13 -0300

    Integracao com BD Padrao DAO Aluno corrigido

--- src/IntegracaoBancoDeDados/Aluno.java
+++ src/IntegracaoBancoDeDados/Aluno.java
@@ -17,22 +17,4 @@ public class Aluno {
         this.ano = ano;
     }
 
-    List<Aluno> lista = new ArrayList<>();
-    Class.forName("org.apache.derby.jdbc.ClientDriver");
-    Connection c1 = DriverManager.getConnection(
-            "jdbc:derby://localhost:1527/loja",
-            "loja", "loja");
-    Statement st = c1.createStatement();
-    ResultSet r1 = st.executeQuery("SELECT * FROM ALUNO");
-    while (r1.next()) {
-            lista.add(new Aluno(
-                    r1.getString("MATRICULA"),
-                    r1.getString("NOME"),
-                    r1.getInt("ENTRADA")
-            ));
-        }
-    r1.close();
-    st.close();
-    c1.close();
-
 }
